b196a97977434361a1f131be81fd77080a4e28f8
Tobias Koch <tobias.koch@gmail.com>
2026-03-14 20:22:30 +0300

    Avoid clashes when two instances download to same cache

--- src/download.c
+++ src/download.c
@@ -15,12 +15,14 @@
 #include "aept/internal.h"
 #include "aept/download.h"
 #include "aept/msg.h"
+#include "aept/util.h"
 
 int aept_download(struct aept_ctx *ctx, const char *url, const char *dest,
                   const char *name)
 {
     fetchIO *fio = NULL;
     FILE *fp = NULL;
+    char *tmp = NULL;
     char buf[8192];
     ssize_t n;
     int ret = -1;
@@ -33,29 +35,44 @@ int aept_download(struct aept_ctx *ctx, const char *url, const char *dest,
     if (ctx->config.ssl_client_key)
         setenv("SSL_CLIENT_KEY_FILE", ctx->config.ssl_client_key, 1);
 
-    unlink(dest);
-
     fio = fetchGetURL(url, "");
     if (!fio) {
         aept_log_error("failed to download '%s'", url);
         return -1;
     }
 
-    fp = fopen(dest, "wb");
+    /* Download to <dest>.<pid>, then rename into place. This ensures
+     * readers never see a partially-written file, even when multiple
+     * aept instances share the same download cache. */
+    aept_asprintf(&tmp, "%s.%d", dest, (int)getpid());
+
+    fp = fopen(tmp, "wb");
     if (!fp) {
-        aept_log_error("cannot create '%s': %s", dest, strerror(errno));
+        aept_log_error("cannot create '%s': %s", tmp, strerror(errno));
         goto cleanup;
     }
 
     while ((n = fetchIO_read(fio, buf, sizeof(buf))) > 0) {
         if (fwrite(buf, 1, n, fp) != (size_t)n) {
-            aept_log_error("write error for '%s': %s", dest, strerror(errno));
+            aept_log_error("write error for '%s': %s", tmp, strerror(errno));
             goto cleanup;
         }
     }
 
     if (n < 0) {
         aept_log_error("failed to download '%s'", url);
+        goto cleanup;
+    }
+
+    if (fclose(fp) != 0) {
+        aept_log_error("write error for '%s': %s", tmp, strerror(errno));
+        fp = NULL;
+        goto cleanup;
+    }
+    fp = NULL;
+
+    if (rename(tmp, dest) != 0) {
+        aept_log_error("rename '%s' -> '%s': %s", tmp, dest, strerror(errno));
         goto cleanup;
     }
 
@@ -66,7 +83,8 @@ cleanup:
         fetchIO_close(fio);
     if (fp)
         fclose(fp);
-    if (ret != 0)
-        unlink(dest);
+    if (ret != 0 && tmp)
+        unlink(tmp);
+    free(tmp);
     return ret;
 }
